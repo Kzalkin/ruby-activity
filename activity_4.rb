e2f7a2cee98d4f5d57e2d6b8b3a13c5e59bcd4ca
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-05-14 09:24:55 +0800

    activity 4

---
+++ activity_4.rb
@@ -0,0 +1,18 @@
+class Confection
+  def prepare
+    puts "Baking at 350 degrees for 25 minutes."
+  end
+end
+
+class Cupcake < Confection
+  def prepare
+    super
+    puts "Applying frosting"
+  end
+end
+
+class BananaCake < Confection
+  def prepare
+    super
+  end
+end
