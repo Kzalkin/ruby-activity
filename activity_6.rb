84837de9d44b3fdecfaab59cc6f24797f5704eec
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-05-17 21:12:53 +0800

    activity 6

--- activity_6.rb
+++ activity_6.rb
@@ -1,15 +1,12 @@
 def lowest(arr)
-    index = 1
-    low = arr[index]
+    low = arr.first
     arr.each do |member|
         if member < low
             low = member
-            index += 1
         end
     end
-    puts low
     low
 end
 
-lowest [34,15,88,2]
-lowest [43,-345,-1,100]
+puts lowest [34,15,88,2]
+puts lowest [43,-345,-1,100]
