74b109dbf3173a6d8595ccbb3c5f50595257fea2
Eglecerio Malunes III <egleceriom425@gmail.com>
2022-05-19 20:02:36 +0800

    db activity 2

---
+++ db_activity2.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(id) AS total_number_of_students FROM students;
+
+SELECT * FROM students WHERE location = 'Manila';
+
+SELECT AVG(age) as average_age FROM students;
+
+SELECT * FROM students ORDER BY age DESC;
